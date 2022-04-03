5ae0108f5800ba7a3ae05ffa30f65a6b93bd61d8
Michael <michaelkatom10@gmail.com>
2022-04-03 21:19:59 +0300

    section 4 done + bug fixes

--- server/src/datasources/user.js
+++ server/src/datasources/user.js
@@ -31,7 +31,7 @@ class UserAPI extends DataSource {
   }
 
   async bookTrips({ launchIds }) {
-    const userId = this.context.user.id;
+    const userId = this.context.user?.id;
     if (!userId) return;
 
     let results = [];
@@ -51,7 +51,7 @@ class UserAPI extends DataSource {
     const res = await this.store.trips.findOrCreate({
       where: { userId, launchId },
     });
-    return res && res.length ? res[0].get() : false;
+    return res?.length ? res[0].get() : false;
   }
 
   async cancelTrip({ launchId }) {
